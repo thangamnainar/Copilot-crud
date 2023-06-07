76a9032b570925ea55feed2f365829e0aaa574be
Thangam N <thangam.nainar0507@gmail.com>
2023-06-07 13:22:49 +0530

    first commit

---
+++ server/server.js
@@ -0,0 +1,117 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var mysql = require("mysql");
+var express = require('express');
+var cors = require('cors');
+var Database = /** @class */ (function () {
+    function Database() {
+        this._connection = mysql.createConnection({
+            host: 'localhost',
+            port: 3306,
+            user: 'thangam',
+            password: 'Thasan24',
+            database: 'test'
+        });
+        this._connection.connect(function (err) {
+            if (err)
+                throw err;
+            console.log('Connected!');
+        });
+    }
+    Object.defineProperty(Database.prototype, "connection", {
+        get: function () {
+            return this._connection;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    return Database;
+}());
+var App = /** @class */ (function () {
+    function App() {
+        var _this = this;
+        this.express = express();
+        this.express.use(express.json());
+        this.express.use(cors());
+        this._db = new Database();
+        this.express.get('/users', function (req, res) { return _this.getUsers(req, res); });
+        this.express.post('/addUsers', function (req, res) { return _this.addUser(req, res); });
+        this.express.get('/getById/:id', function (req, res) { return _this.getById(req, res); });
+        this.express.put('/updateUser/:id', function (req, res) { return _this.updateUser(req, res); });
+        this.express.put('/deleteUser/:id', function (req, res) { return _this.deleteUser(req, res); });
+        this.listen();
+    }
+    App.prototype.getUsers = function (req, res) {
+        this._db.connection.query('SELECT name,age,gender,skills FROM student_details where isActive = 0', function (err, result) {
+            if (err) {
+                console.log(err);
+            }
+            else {
+                if (result.length > 0) {
+                    res.json(result);
+                }
+                else {
+                    res.json({ message: "No User Found" });
+                }
+            }
+        });
+    };
+    App.prototype.getById = function (req, res) {
+        var id = req.params.id;
+        this._db.connection.query('SELECT name,age,gender,skills FROM student_details WHERE id = ?', id, function (err, result) {
+            if (err) {
+                console.log(err);
+            }
+            else {
+                if (result.length > 0) {
+                    res.json(result);
+                    console.log(result);
+                }
+                else {
+                    res.json({ message: "No User Found" });
+                }
+            }
+        });
+    };
+    App.prototype.addUser = function (req, res) {
+        var user = req.body;
+        this._db.connection.query('INSERT INTO student_details SET ?', user, function (err, result) {
+            if (err) {
+                console.log(err);
+            }
+            else {
+                res.json({ message: "User Added Successfully" });
+            }
+        });
+    };
+    App.prototype.updateUser = function (req, res) {
+        var id = req.params.id;
+        var updateData = req.body;
+        this._db.connection.query('UPDATE student_details SET ? WHERE id = ?', [updateData, id], function (err, result) {
+            if (err) {
+                console.log(err);
+            }
+            else {
+                res.json({ message: "User Updated Successfully" });
+            }
+        });
+    };
+    App.prototype.deleteUser = function (req, res) {
+        var id = req.params.id;
+        this._db.connection.query('UPDATE student_details SET isActive = 1 WHERE id = ?', id, function (err, result) {
+            if (err) {
+                console.log(err);
+            }
+            else {
+                res.json({ message: "User Deleted Successfully" });
+            }
+        });
+    };
+    App.prototype.listen = function () {
+        this.express.listen(3000, function () {
+            console.log('Server running on port 3000');
+        });
+    };
+    return App;
+}());
+var app = new App();
---
+++ server/server.ts
@@ -0,0 +1,110 @@
+import { Express, Request, Response } from 'express';
+import * as mysql from 'mysql';
+
+const express = require('express');
+const cors = require('cors');
+
+class Database {
+    private _connection: mysql.Connection;
+    constructor() {
+        this._connection = mysql.createConnection({
+            host: 'localhost',
+            port: 3306,
+            user: 'thangam',
+            password: 'Thasan24',
+            database: 'test'
+        });
+        this._connection.connect((err) => {
+            if (err) throw err;
+            console.log('Connected!');
+        });
+    }
+    get connection(): mysql.Connection {
+        return this._connection;
+    }
+}
+
+class App {
+    public express: Express;
+    private _db: Database;
+    constructor() {
+        this.express = express();
+        this.express.use(express.json());
+        this.express.use(cors());
+        this._db = new Database();
+        this.express.get('/users', (req: Request, res: Response) => this.getUsers(req, res));
+        this.express.post('/addUsers', (req: Request, res: Response) => this.addUser(req, res));
+        this.express.get('/getById/:id', (req: Request, res: Response) => this.getById(req, res));
+        this.express.put('/updateUser/:id', (req: Request, res: Response) => this.updateUser(req, res));
+        this.express.put('/deleteUser/:id', (req: Request, res: Response) => this.deleteUser(req, res));
+        this.listen();
+    }
+    private getUsers(req: Request, res: Response) {
+        this._db.connection.query('SELECT name,age,gender,skills FROM student_details where isActive = 0', (err, result) => {
+            if (err) {
+                console.log(err);
+            } else {
+                if (result.length > 0) {
+                    res.json(result);
+                } else {
+                    res.json({ message: "No User Found" });
+                }
+            }
+        });
+    }
+    private getById(req: Request, res: Response) {
+        let id = req.params.id;
+        this._db.connection.query('SELECT name,age,gender,skills FROM student_details WHERE id = ?', id, (err, result) => {
+            if (err) {
+                console.log(err);
+            } else {
+                if (result.length > 0) {
+                    res.json(result);
+                    console.log(result);
+                } else {
+                    res.json({ message: "No User Found" });
+                    
+                }
+            }
+        });
+    }
+    private addUser(req: Request, res: Response) {
+        let user = req.body;
+        this._db.connection.query('INSERT INTO student_details SET ?', user, (err, result) => {
+            if (err) {
+                console.log(err);
+            } else {
+                res.json({ message: "User Added Successfully" });
+            }
+        });
+    }
+    private updateUser(req: Request, res: Response) {
+        let id = req.params.id;
+        let updateData = req.body;
+        this._db.connection.query('UPDATE student_details SET ? WHERE id = ?', [updateData, id], (err, result) => {
+            if (err) {
+                console.log(err);
+            } else {
+                res.json({ message: "User Updated Successfully" });
+            }
+        });
+    }
+    private deleteUser(req: Request, res: Response) {
+        let id = req.params.id;
+        this._db.connection.query('UPDATE student_details SET isActive = 1 WHERE id = ?', id, (err, result) => {
+            if (err) {
+                console.log(err);
+            } else {
+                res.json({ message: "User Deleted Successfully" });
+            }
+        });
+    }
+    public listen(): void {
+        this.express.listen(3000, () => {
+            console.log('Server running on port 3000');
+        });
+    }
+
+
+}
+let app = new App();
